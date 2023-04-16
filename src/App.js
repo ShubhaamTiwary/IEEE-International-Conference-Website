099bb0414556ff0a6a0821617575413f9042beed
Shubhaam Tiwary <shubhaamtiwary@gmail.com>
2023-04-16 18:54:14 +0530

    Folder Structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Hello</h1>
   );
 }
 
 export default App;
+
+// Header 
+// Navber
+
+// home == > 1.image slider
+      //     2.Text slider
+      //     3.About Conference
+      //     4. Contact Details
+      //     5. Map
+
+// Footer
+
+
