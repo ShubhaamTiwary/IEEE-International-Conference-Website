f0c155ae33b732119bd697517a074e4a2515b012
Shubhaam Tiwary <shubhaamtiwary@gmail.com>
2023-04-29 02:49:51 +0530

    done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -51,50 +51,34 @@ const Navbar = () => {
 				<a href="#">HOME</a>
 			</li>
 			<li>
-				<a href="#">FOR AUTHORS</a>
-				<ul class="dropdown-list">
-					<li><a href="#">Call for Paper</a></li>
-					<li><a href="#">Important Dates</a></li>
-				</ul>
-			</li>
-			<li>
-				<a href="#">SPECIAL SESSION</a>
-				<ul class="dropdown-list">
-					<li><a href="#">SPECIAL SESSION-1</a></li>
-					<li><a href="#">SPECIAL SESSION-2</a></li>
-					<li><a href="#">SPECIAL SESSION-3</a></li>
-				</ul>
+				<a href="#">COMMITTEE</a>
 			</li>
 			<li>
 				<a href="#">KEYNOTES</a>
 			</li>
 			<li>
-				<a href="#">TUTORIALS</a>
-				<ul class="dropdown-list">
-					<li><a href="#">TUTORIALS-1</a></li>
-					<li><a href="#">TUTORIALS-2</a></li>
-					<li><a href="#">TUTORIALS-3</a></li>
-				</ul>
-			</li>
-			<li>
-				<a href="#">SCHEDULE</a>
-			</li>
-			<li>
 				<a href="#">REGISTRATION</a>
 			</li>
 			<li>
-				<a href="#">SPONSORSHIP</a>
-			</li>
-			<li>
-				<a href="#">COMMITTEES</a>
-			</li>
-			<li>
-				<a href="#">CONTACT US</a>
+				<a href="#">PAPER SUBMISSION</a>
 				<ul class="dropdown-list">
-					<li><a href="#">CONTACT US-1</a></li>
-					<li><a href="#">CONTACT US-2</a></li>
-					<li><a href="#">CONTACT US-3</a></li>
+					<li><a href="#">Authors Guidelines</a></li>
+					<li><a href="#">Paper Template</a></li>
+					<li><a href="#">Schedule</a></li>
+					<li><a href="#">FAQs</a></li>
 				</ul>
+			</li>
+			<li>
+				<a href="#">CALL FOR PAPERS</a>
+			</li>
+			<li>
+				<a href="#">PUBLICATION</a>
+			</li>
+			<li>
+				<a href="#">SPECIAL SESSION</a>
+			</li>
+			<li>
+				<a href="#">OUR ASSOCIATE & SPONSORS </a>
 			</li>
 		</ul>
 	</div>
